b7097684b6a3cb9f5f502ba81f1d55411d409891
viste <viste02@gmail.com>
2024-04-07 15:33:49 +0300

    update admin login

--- portal/app.py
+++ portal/app.py
@@ -135,10 +135,16 @@ class LoginForm(form.Form):
         user = self.get_user()
 
         if user is None:
-            raise validators.ValidationError('Invalid user')
+            raise validators.ValidationError('Вы не зарегистрированы')
 
-        if not check_password_hash(user.password_hash, self.password.data):
-            raise validators.ValidationError('Invalid password')
+        if not check_password_hash(user.password, self.password.data):
+            raise validators.ValidationError('Неправильный Пароль')
+
+        if user.is_admin is False:
+            raise validators.ValidationError('Вы не администратор')
+
+        if user.is_moderator is False:
+            raise validators.ValidationError('Вы не модератор')
 
     def get_user(self):
         return db.session.query(Customer).filter_by(username=self.login.data).first()
