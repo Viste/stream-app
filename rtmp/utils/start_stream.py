4fec1bfb1db05126e4b1530a815d591c5ce3d96a
viste <viste02@gmail.com>
2024-04-15 23:30:18 +0300

    update
    update

--- rtmp/utils/start_stream.py
+++ rtmp/utils/start_stream.py
@@ -1,16 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env python3
+import sys
+import requests
+import json
+import logging
 
-COURSE_NAME=$1
+logging.basicConfig(filename='/home/logfile.log', level=logging.DEBUG)
 
-RESPONSE=$(curl -s -X POST http://academy-service.stream.svc.pprfnk.local/api/start_broadcast \
-     -H "Content-Type: application/json" \
-     -d "{\"short_name\":\"$COURSE_NAME\"}")
 
-echo $RESPONSE > "/home/response.log"
+def start_stream(course_name):
+    logging.debug("Starting stream for %s", course_name)
+    url = "http://academy-service.stream.svc.pprfnk.local/api/start_broadcast"
+    headers = {"Content-Type": "application/json"}
+    data = json.dumps({"short_name": course_name})
+    response = requests.post(url, headers=headers, data=data)
+    broadcast_id = response.json().get('broadcast_id')
 
-BROADCAST_ID=$(echo $RESPONSE | jq -r '.broadcast_id')
+    with open(f"/home/{course_name}_broadcast_id.txt", "w") as f:
+        f.write(str(broadcast_id))
 
-echo "BROADCAST ID: $BROADCAST_ID"
+    logging.debug("BROADCAST ID: %s", broadcast_id)
+    logging.debug("Stream started for %s", course_name)
 
-echo $BROADCAST_ID > "/home/${COURSE_NAME}_broadcast_id.txt"
 
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        start_stream(sys.argv[1])
