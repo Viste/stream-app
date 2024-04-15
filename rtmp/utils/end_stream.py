766b767d2e4c59a69c11116bb95f0f8b80948667
viste <viste02@gmail.com>
2024-04-15 22:35:17 +0300

    bash -> python

---
+++ rtmp/utils/end_stream.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+import requests
+import json
+import shutil
+
+def end_stream(course_name):
+    with open(f"/home/{course_name}_broadcast_id.txt", "r") as f:
+        broadcast_id = f.read().strip()
+
+    unique_video_name = f"{course_name}_{broadcast_id}.flv"
+    unique_video_path = f"/home/storage/{unique_video_name}"
+    shutil.copy(f"/home/storage/{course_name}.flv", unique_video_path)
+
+    url = "http://academy-service.stream.svc.pprfnk.local/api/end_broadcast"
+    headers = {"Content-Type": "application/json"}
+    data = json.dumps({"broadcast_id": broadcast_id, "video_path": unique_video_name})
+    requests.post(url, headers=headers, data=data)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        end_stream(sys.argv[1])
