c86a82572015a9a7058ef0c4480d1276162eef20
viste <viste02@gmail.com>
2024-04-16 04:28:25 +0300

    convert videos to mp4 at stream end

--- rtmp/utils/end_stream.py
+++ rtmp/utils/end_stream.py
@@ -2,20 +2,28 @@
 import sys
 import requests
 import json
-import shutil
+import subprocess
+
+
+def convert_flv_to_mp4(flv_path, mp4_path):
+    command = ['ffmpeg', '-i', flv_path, '-codec', 'copy', mp4_path]
+    subprocess.run(command, check=True)
 
 
 def end_stream(course_name):
     with open(f"/home/{course_name}_broadcast_id.txt", "r") as f:
         broadcast_id = f.read().strip()
 
-    unique_video_name = f"{course_name}_{broadcast_id}.flv"
-    unique_video_path = f"/home/storage/{unique_video_name}"
-    shutil.copy(f"/home/storage/{course_name}.flv", unique_video_path)
+    base_video_path = f"/home/storage/{course_name}.flv"
+
+    # Конвертация в MP4
+    unique_video_name_mp4 = f"{course_name}_{broadcast_id}.mp4"
+    unique_video_path_mp4 = f"/home/storage/{unique_video_name_mp4}"
+    convert_flv_to_mp4(base_video_path, unique_video_path_mp4)
 
     url = "https://academy.pprfnk.tech/api/end_broadcast"
     headers = {"Content-Type": "application/json", "X-API-Key": "pprfkebetvsehrot2024"}
-    data = json.dumps({"broadcast_id": broadcast_id, "video_path": unique_video_name})
+    data = json.dumps({"broadcast_id": broadcast_id, "video_path": unique_video_name_mp4})
     requests.post(url, headers=headers, data=data)
 
 
