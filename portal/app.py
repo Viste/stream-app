8c89805907154b54d725c1799caaeebeb515d850
viste <viste02@gmail.com>
2024-04-18 02:41:43 +0300

    try to fix profile
    Signed-off-by: viste <viste02@gmail.com>

--- portal/app.py
+++ portal/app.py
@@ -182,6 +182,7 @@ def profile():
         allowed_course_short_names = current_user.allowed_courses.split(',')
         user_courses = Course.query.filter(Course.short_name.in_(allowed_course_short_names)).all()
         submissions = HomeworkSubmission.query.filter_by(student_id=current_user.id).join(Homework, Homework.id == HomeworkSubmission.homework_id).add_columns(Homework.course_id, HomeworkSubmission.grade, HomeworkSubmission.comments).all()
+        print(submissions)
         return render_template('profile.html', account=current_user, courses=user_courses, submissions=submissions)
     return redirect(url_for('login'))
 
