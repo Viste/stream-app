c7d52e3eee7089f155a16d4517e9bd4e53e8ca30
viste <viste02@gmail.com>
2024-04-21 03:11:12 +0300

    blya ya v ahue
    Signed-off-by: viste <viste02@gmail.com>

--- portal/app.py
+++ portal/app.py
@@ -16,6 +16,7 @@ from wtforms import form, fields, validators
 from wtforms.fields import TextAreaField, IntegerField
 
 app = Flask(__name__)
+app.config["JWT_TOKEN_LOCATION"] = ["query_string"]
 app.config['JWT_ALGORITHM'] = 'HS256'
 app.config['JWT_SECRET_KEY'] = 'pprfnktechsekta2024'
 app.config['SECRET_KEY'] = 'pprfnktechsekta2024'
