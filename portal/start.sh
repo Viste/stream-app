dedfd4214057dd26b09b2a8b58469f26451822da
viste <viste02@gmail.com>
2024-04-26 22:51:47 +0300

    add migrations close #16 and rise gunicorn workers to procnum
    Signed-off-by: viste <viste02@gmail.com>

--- portal/start.sh
+++ portal/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Запуск сервера Node.js в фоне
+echo "Running database migrations..."
+flask db upgrade
+
+echo "Running node server for player..."
 node server.js &
 
-# Запуск Gunicorn на переднем плане
-exec gunicorn -b 0.0.0.0:5000 -w 8 main:app
+echo "Running node server for player..."
+exec gunicorn -b 0.0.0.0:5000 -w 32 main:app
