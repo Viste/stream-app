b61750cc3c5fcddaf60324ba36a766cf44ce908d
viste <viste02@gmail.com>
2024-05-11 20:40:38 +0300

    lol
    Signed-off-by: viste <viste02@gmail.com>

--- portal/app.py
+++ portal/app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 
 from api.api import api
 from core.admin.admin_panel import admin
+from core.mod.mod_panel import mod
 from core.portal import views
 from database.models import db
 from tools.auth import login_manager
@@ -19,6 +20,7 @@ jwt = JWTManager(app)
 app.register_blueprint(views)
 app.register_blueprint(api, url_prefix='/api')
 admin.init_app(app)
+mod.init_app(app)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, use_reloader=False)
