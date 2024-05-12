d4135748da8e475ba0e42327a6f26e1e246227fb
Mizhory <mizhory@gmail.com>
2024-05-12 11:36:36 +0300

    Верстка. Доработки на проде 12.05.24 11:30 Курсы

--- static/pprfnk_dsgn/script.js
+++ static/pprfnk_dsgn/script.js
@@ -52,4 +52,12 @@ $(document).ready(() => {
             }
         });
     }
+    let triggerTabList = [].slice.call(document.querySelectorAll('#coursestablist a'))
+    triggerTabList.forEach(function (triggerEl) {
+        let tabTrigger = new bootstrap.Tab(triggerEl)
+        triggerEl.addEventListener('click', function (event) {
+            event.preventDefault()
+            tabTrigger.show()
+        })
+    })
 })
