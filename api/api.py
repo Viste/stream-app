09b8ece8595a6a0faa3f5d013f3bfba3fca622b8
viste <viste02@gmail.com>
2024-05-16 18:36:20 +0300

    fix api
    Signed-off-by: viste <viste02@gmail.com>

--- api/api.py
+++ api/api.py
@@ -9,15 +9,14 @@ api = Blueprint('api', __name__)
 @api.route('/start_broadcast', methods=['POST'])
 @require_api_key
 def start_broadcast():
-    global next_broadcast_title
     data = request.json
     course = Course.query.filter_by(short_name=data['short_name']).first()
     if course:
-        title = next_broadcast_title if next_broadcast_title else f"Трансляция курса: {course.name}"
-        new_broadcast = Broadcast(course_id=course.id, is_live=True, title=title)
+        # title = next_broadcast_title if next_broadcast_title else f"Трансляция курса: {course.name}"
+        new_broadcast = Broadcast(course_id=course.id, is_live=True)
         db.session.add(new_broadcast)
         db.session.commit()
-        return jsonify({"message": "Broadcast started successfully", "broadcast_id": new_broadcast.id, "title": new_broadcast.title}), 200
+        return jsonify({"message": "Broadcast started successfully", "broadcast_id": new_broadcast.id}), 200
     return jsonify({"message": "Course not found"}), 404
 
 
