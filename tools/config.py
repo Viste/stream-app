3a3d500b4a240a5554bd5242bc76d6441199b58e
viste <viste02@gmail.com>
2024-06-06 19:26:58 +0300

    lol
    Signed-off-by: viste <viste02@gmail.com>
    
    no cache
    
    Signed-off-by: viste <viste02@gmail.com>
    
    push
    
    Signed-off-by: viste <viste02@gmail.com>
    
    remove gitea deploy and update readme
    
    upd

--- tools/config.py
+++ tools/config.py
@@ -1,14 +1,14 @@
 class Config:
     JWT_TOKEN_LOCATION = ["query_string"]
-    JWT_ALGORITHM = 'HS256'
-    JWT_SECRET_KEY = 'pprfnktechsekta2024'
-    SECRET_KEY = 'pprfnktechsekta2024'
-    API_KEY = 'pprfkebetvsehrot2024'
-    UPLOAD_FOLDER = '/app/static/storage'
-    SQLALCHEMY_DATABASE_URI = 'mariadb+pymysql://sysop:0Z3tcFg7FE60YBpKdquwrQRk@pprfnkdb-primary.mariadb.svc.pprfnk.local/cyber?charset=utf8mb4'
+    JWT_ALGORITHM = "HS256"
+    JWT_SECRET_KEY = ""
+    SECRET_KEY = ""
+    API_KEY = ""
+    UPLOAD_FOLDER = "/app/static/storage"
+    SQLALCHEMY_DATABASE_URI = "mariadb+pymysql://USER:PASS@HOST/BASE?charset=utf8mb4"
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_POOL_SIZE = 10
     SQLALCHEMY_POOL_TIMEOUT = 30
     SQLALCHEMY_POOL_RECYCLE = 1800
     SQLALCHEMY_MAX_OVERFLOW = 5
-    SQLALCHEMY_ENGINE_OPTIONS = {'pool_pre_ping': True}
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
